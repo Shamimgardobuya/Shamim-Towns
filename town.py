62a0a55429ca6827e26abdd0398a742005767989
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-18 23:12:04 +0300

    Creating a function

---
+++ town.py
@@ -0,0 +1,5 @@
+def urban_centres(city):
+    city={"Delhi":"Red Fort","Jaipur":"Jal Mahal"}
+a=str(input("Enter the City you'd like to be travel to"))
+if a=="Delhi":
+    print(f"You'd like to go to {city[]} ")
