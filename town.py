5d7ad8944146f9bdaadef8ce158ae3c400ac6f79
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-19 21:18:42 +0300

    city of India

--- town.py
+++ town.py
@@ -1,5 +1,11 @@
-def urban_centres(city):
-    city={"Delhi":"Red Fort","Jaipur":"Jal Mahal"}
-a=str(input("Enter the City you'd like to be travel to"))
+# def urban_centres(c
+city={"Delhi":"Red Fort","Jaipur":"Tal Mahal"}
+a=str(input("Enter the City you'd like to be travel to "))
 if a=="Delhi":
-    print(f"You'd like to go to {city[]} ")
+        print(city["Delhi"] ,f" is a nice place to go")#i actually needed a comma to seperate though need to know how to use format with dictionaries
+elif a=="Jaipur":
+        print(city["Jaipur"],f"is a nice place to go")
+# a=city
+# urban_centres(city) 
+#get used to functions.
+    
